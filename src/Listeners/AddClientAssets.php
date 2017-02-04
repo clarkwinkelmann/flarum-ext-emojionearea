114015fcf1c33d9eb23243aacb4473b16f158438
Clark Winkelmann <clark.winkelmann@gmail.com>
2017-02-04 00:20:24 +0000

    Apply fixes from StyleCI

--- src/Listeners/AddClientAssets.php
+++ src/Listeners/AddClientAssets.php
@@ -2,7 +2,7 @@
 
 /**
  * This file is part of clarkwinkelmann/flarum-ext-emojionearea
- * See README.md for details and license
+ * See README.md for details and license.
  */
 
 namespace ClarkWinkelmann\EmojiOneArea\Listeners;
@@ -12,16 +12,18 @@ use Illuminate\Contracts\Events\Dispatcher;
 
 class AddClientAssets
 {
-    public function subscribe(Dispatcher $events) {
+    public function subscribe(Dispatcher $events)
+    {
         $events->listen(ConfigureClientView::class, [$this, 'configureClientView']);
     }
 
-    public function configureClientView(ConfigureClientView $event) {
+    public function configureClientView(ConfigureClientView $event)
+    {
         if ($event->isForum()) {
             $event->addAssets([
-                __DIR__ . '/../../js/forum/dist/extension.js',
-                __DIR__ . '/../../less/forum/extension.less',
-                __DIR__ . '/../../css/forum/dist/emojionearea.css',
+                __DIR__.'/../../js/forum/dist/extension.js',
+                __DIR__.'/../../less/forum/extension.less',
+                __DIR__.'/../../css/forum/dist/emojionearea.css',
             ]);
             $event->addBootstrapper('clarkwinkelmann/emojionearea/main');
         }
